9acb4024ae360a261b31fe350e0bae28482033a1
Mkrukov <mcshake1000@gmail.com>
2023-09-14 11:55:38 +0300

    Add tests for deposit-service

--- deposit-service/src/main/java/com/javastart/gateway/repository/DepositRepository.java
+++ deposit-service/src/main/java/com/javastart/gateway/repository/DepositRepository.java
@@ -3,6 +3,9 @@ package com.javastart.gateway.repository;
 import com.javastart.gateway.entity.Deposit;
 import org.springframework.data.repository.CrudRepository;
 
+import java.util.List;
+
 public interface DepositRepository extends CrudRepository<Deposit, Long> {
 
+    List<Deposit> findDepositsByEmail(String email);
 }
---
+++ deposit-service/src/test/java/com/javastart/depositservice/service/DepositServiceTest.java
@@ -0,0 +1,78 @@
+package com.javastart.depositservice.service;
+
+import com.javastart.gateway.controller.dto.DepositResponseDTO;
+import com.javastart.gateway.exception.DepositServiceException;
+import com.javastart.gateway.repository.DepositRepository;
+import com.javastart.gateway.rest.AccountResponseDTO;
+import com.javastart.gateway.rest.AccountServiceClient;
+import com.javastart.gateway.rest.BillResponseDTO;
+import com.javastart.gateway.rest.BillServiceClient;
+import com.javastart.gateway.service.DepositService;
+import org.assertj.core.api.Assertions;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.ArgumentMatchers;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.Mockito;
+import org.mockito.junit.MockitoJUnitRunner;
+import org.springframework.amqp.rabbit.core.RabbitTemplate;
+
+import java.math.BigDecimal;
+import java.time.OffsetDateTime;
+import java.util.Arrays;
+
+@RunWith(MockitoJUnitRunner.class)
+public class DepositServiceTest {
+
+    @Mock
+    private DepositRepository depositRepository;
+
+    @Mock
+    private AccountServiceClient accountServiceClient;
+
+    @Mock
+    private BillServiceClient billServiceClient;
+
+    @Mock
+    private RabbitTemplate rabbitTemplate;
+
+    @InjectMocks
+    private DepositService depositService;
+
+    @Test
+    public void depositServiceTest_withBillId() {
+        BillResponseDTO billResponseDTO = createBillResponseDTO();
+        Mockito.when(billServiceClient.getBillById(ArgumentMatchers.anyLong())).thenReturn(billResponseDTO);
+        Mockito.when(accountServiceClient.getAccountById(ArgumentMatchers.anyLong())).thenReturn(createAccountResponseDTO());
+        DepositResponseDTO deposit = depositService.deposit(null, 1L, BigDecimal.valueOf(1000));
+        Assertions.assertThat(deposit.getMail()).isEqualTo("lori@cat.xyz");
+    }
+
+    @Test(expected = DepositServiceException.class)
+    public void depositServiceTest_exception() {
+        depositService.deposit(null,null, BigDecimal.valueOf(1000));
+    }
+
+    private AccountResponseDTO createAccountResponseDTO() {
+        AccountResponseDTO accountResponseDTO = new AccountResponseDTO();
+        accountResponseDTO.setAccountId(1L);
+        accountResponseDTO.setBills(Arrays.asList(1L, 2L, 3L));
+        accountResponseDTO.setCreationDate(OffsetDateTime.now());
+        accountResponseDTO.setEmail("lori@cat.xyz");
+        accountResponseDTO.setName("Lori");
+        accountResponseDTO.setPhone("+144256745");
+        return accountResponseDTO;
+    }
+
+    private BillResponseDTO createBillResponseDTO() {
+        BillResponseDTO billResponseDTO = new BillResponseDTO();
+        billResponseDTO.setAccountId(1L);
+        billResponseDTO.setAmount(BigDecimal.valueOf(1000));
+        billResponseDTO.setBillId(1L);
+        billResponseDTO.setCreationDate(OffsetDateTime.now());
+        billResponseDTO.setIsDefault(true);
+        billResponseDTO.setOverdraftEnabled(true);
+        return billResponseDTO;
+    }
+}
